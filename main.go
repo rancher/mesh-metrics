b364a2df600d05ba97be64d561e99a693f4b8e7b
Dax McDonald <dax@rancher.com>
2019-11-12 14:56:40 -0700

    Drop k8s API bringup
    This might be added back later but for now we do not need it

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,7 +12,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/linkerd/linkerd2/pkg/admin"
 	"github.com/linkerd/linkerd2/pkg/flags"
-	"github.com/linkerd/linkerd2/pkg/k8s"
 	"github.com/prometheus/client_golang/api"
 	"github.com/sirupsen/logrus"
 )
@@ -23,17 +21,17 @@ func main() {
 	cmd := flag.NewFlagSet("public-api", flag.ExitOnError)
 	addr := cmd.String("addr", "127.0.0.1:8084", "address to serve on")
 	metricsAddr := cmd.String("metrics-addr", "127.0.0.1:9958", "address to serve scrapable metrics on")
-	apiAddr := cmd.String("api-addr", "127.0.0.1:9090", "address of the prometheus service")
+	apiAddr := cmd.String("api-addr", "http://linkerd-prometheus.linkerd.svc.cluster.local:9090", "address of the prometheus service")
 	prometheusNamespace := cmd.String("controller-namespace", "linkerd", "namespace in which Linkerd is installed")
 	debug := cmd.Bool("debug", false, "enable debug logging")
-	kubeConfigPath := cmd.String("kubeconfig", "", "path to kube config")
+	//kubeConfigPath := cmd.String("kubeconfig", "", "path to kube config")
 
 	flags.ConfigureAndParse(cmd, os.Args[1:])
 
 	if *debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
-	// TODO maybe sanity check we can connect here?
+	// TODO Sanity check we can connect here and verify promtheus server has correct config
 
 	client, err := api.NewClient(api.Config{
 		Address: *apiAddr,
@@ -42,16 +40,19 @@ func main() {
 		logrus.Fatalf("failed to construct client for API server URL %s", *apiAddr)
 	}
 
-	k8sAPI, err := k8s.NewAPI(*kubeConfigPath, "", "", 0)
-	if err != nil {
-		logrus.Fatalf("failed to construct Kubernetes API client: [%s]", err)
-	}
+	//TODO Figure out a better
+	clusterDomain := "cluster.local"
+
+	// k8sAPI, err := k8s.NewAPI(*kubeConfigPath, "", "", 0)
+	// if err != nil {
+	// 	logrus.Fatalf("failed to construct Kubernetes API client: [%s]", err)
+	// }
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	uuidstring := uuid.New().String()
 
-	server := srv.NewServer(*addr, *prometheusNamespace, "127.0.0.1", true, uuidstring, client, k8sAPI)
+	server := srv.NewServer(*addr, *prometheusNamespace, clusterDomain, true, uuidstring, client)
 
 	go func() {
 		logrus.Infof("starting HTTP server on %+v", *addr)
