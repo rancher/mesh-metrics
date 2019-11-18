f4094ac64060b228c39b6d8c97a1140f8898388a
Dax McDonald <dax@rancher.com>
2019-11-17 19:07:00 -0700

    Implement async queries
    The new summary handler can now make queries in constant time instead
    of making another set of stat queries for every node seen on a set of
    edges.
    
    Also adds a /namespace handler to query all namespaces with the previous
    method to test for regressions.

--- srv/const.go
+++ srv/const.go
@@ -1,7 +1,10 @@
 package srv
 
 const (
-	windowDefault          = "30s"
+	windowDefault     = "30s"
+	directionInbound  = "inbound"
+	directionOutbound = "outbound"
+
 	clusterMemoryUsage     = `sum (container_memory_working_set_bytes{id="/"}) / sum (machine_memory_bytes{}) * 100`
 	clusterCPUUsage1MinAvg = `sum (rate (container_cpu_usage_seconds_total{id="/",}[1m])) / sum (machine_cpu_cores) * 100`
 	clusterFilesytemUse    = `sum (container_fs_usage_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/"}) / sum (container_fs_limit_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/",}) * 100`
@@ -13,9 +16,11 @@ const (
 	promLatencyP50 = "0.5"
 	promLatencyP95 = "0.95"
 	promLatencyP99 = "0.99"
+	RPS            = `sum(irate(request_total%s[%s])) by (app,version,namespace)`
+	SUCCESSRATE    = `sum(irate(response_total%s[%s])) by (app,version,namespace) / sum(irate(response_total%s[%s])) by (app,version,namespace)`
 
-	RPS         = `sum(irate(request_total%s[%s])) by (app)`
-	SUCCESSRATE = `sum(irate(response_total%s[%s])) by (app) / sum(irate(response_total%s[%s])) by (app)`
+	rps         = "rps"
+	successRate = "successRate"
 
 	DEBUG2 = `sum(irate(response_total{classification="success",  direction="inbound"}[30s])) by (app) / sum(irate(response_total{ direction="inbound"}[30s])) by (app)`
 	DEBUG3 = `histogram_quantile(0.5, sum(irate(response_latency_ms_bucket{direction="inbound"}[30s])) by (le,app))`
--- srv/stats.go
+++ srv/stats.go
@@ -3,10 +3,10 @@ package srv
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
-
 	"github.com/prometheus/common/model"
 	"github.com/sirupsen/logrus"
 )
@@ -20,7 +20,10 @@ type promResult struct {
 var quantiles = [3]string{promLatencyP50, promLatencyP95, promLatencyP99}
 
 func statQuery(ctx context.Context, promAPI v1.API, appName, version, window, direction string) (map[string]float64, error) {
-	resultChan := make(chan promResult)
+	if window == "" {
+		window = windowDefault
+	}
+	resultChan := make(chan promResult, len(quantiles))
 	var queryLabels string
 	if version != "" {
 		queryLabels = fmt.Sprintf("{direction=\"%s\",app=\"%s\",version=\"%s\"}", direction, appName, version)
@@ -43,7 +46,6 @@ func statQuery(ctx context.Context, promAPI v1.API, appName, version, window, di
 			}
 			if latencyResult.Type() != model.ValVector {
 				err := fmt.Errorf("unexpected query result type (expected Vector): %s", latencyResult.Type())
-				logrus.Errorf("%v", err)
 				resultChan <- promResult{
 					prom: "",
 					vec:  nil,
@@ -142,4 +144,196 @@ func statQuery(ctx context.Context, promAPI v1.API, appName, version, window, di
 		resp["successRate"] = float64(resultScalar[0].Value)
 	}
 	return resp, nil
+}
+
+const appVersionDeployment = "app,version,deployment"
+
+type StatResp struct {
+	direction string
+	result    map[string]model.Vector
+	err       error
+}
+
+func asyncStatQuery(ctx context.Context, promAPI v1.API, direction, window string, c chan StatResp) {
+	if window == "" {
+		window = windowDefault
+	}
+	statresp := StatResp{
+		direction: direction,
+		result:    nil,
+		err:       nil}
+
+	resultChan := make(chan promResult, len(quantiles))
+	queryLabels := fmt.Sprintf("{direction=\"%s\"}", direction)
+
+	for _, quantile := range quantiles {
+		go func(quantile string) {
+			latencyQuery := fmt.Sprintf(latencyQuantileQuery, quantile, queryLabels, window, appVersionDeployment)
+			logrus.Debugf("Performing stat query: %v", latencyQuery)
+			latencyResult, warnings, err := promAPI.Query(ctx, latencyQuery, time.Now())
+			if err != nil {
+				resultChan <- promResult{
+					prom: "",
+					vec:  nil,
+					err:  err,
+				}
+				return
+			}
+			if latencyResult.Type() != model.ValVector {
+				err := fmt.Errorf("unexpected query result type (expected Vector): %s", latencyResult.Type())
+				resultChan <- promResult{
+					prom: "",
+					vec:  nil,
+					err:  err,
+				}
+				return
+			}
+			if warnings != nil {
+				logrus.Warnf("%v", warnings)
+			}
+
+			resultChan <- promResult{
+				prom: quantile,
+				vec:  latencyResult.(model.Vector),
+				err:  err,
+			}
+		}(quantile)
+	}
+
+	resp := make(map[string]model.Vector)
+	var err error
+	for i := 0; i < len(quantiles); i++ {
+		result := <-resultChan
+		if result.err != nil {
+			logrus.Errorf("query failed with %s", result.err)
+			err = result.err
+		} else {
+			var label string
+			switch result.prom {
+			case promLatencyP50:
+				label = "p50ms"
+			case promLatencyP95:
+				label = "p90ms"
+			case promLatencyP99:
+				label = "p99ms"
+			default:
+				label = result.prom
+			}
+			if len(result.vec) == 0 {
+				resp[label] = nil
+			} else {
+				resp[label] = result.vec
+			}
+		}
+	}
+	if err != nil {
+		// only return if all queries succeeded?
+		statresp.err = err
+		c <- statresp
+		return
+	}
+
+	rpsQuery := fmt.Sprintf(RPS, queryLabels, window)
+	logrus.Debugf("Performing rps query: %v", rpsQuery)
+	result, warnings, err := promAPI.Query(ctx, rpsQuery, time.Now())
+	if err != nil {
+		c <- StatResp{}
+	}
+	if len(warnings) > 0 {
+		logrus.Warnf("%v", warnings)
+	}
+	resultVector, ok := result.(model.Vector)
+	if !ok {
+		statresp.err = fmt.Errorf("unexpected query result type (expected Vector): %s", result.Type())
+		c <- statresp
+		return
+	}
+	if len(resultVector) == 0 {
+		resp["rps"] = nil
+	} else {
+		resp["rps"] = resultVector
+	}
+
+	var nonSuccesRateLabels string
+
+	queryLabels = fmt.Sprintf("{classification=\"success\",direction=\"%s\"}", direction)
+	nonSuccesRateLabels = fmt.Sprintf("{direction=\"%s\"}", direction)
+
+	successRateQuery := fmt.Sprintf(SUCCESSRATE, queryLabels, window, nonSuccesRateLabels, window)
+	logrus.Debugf("Performing success rate query: %v", successRateQuery)
+	result, warnings, err = promAPI.Query(ctx, successRateQuery, time.Now())
+	if err != nil {
+		statresp.err = err
+		c <- statresp
+		return
+	}
+	if len(warnings) > 0 {
+		logrus.Warnf("%v", warnings)
+	}
+	resultVector, ok = result.(model.Vector)
+	if !ok {
+		statresp.err = fmt.Errorf("unexpected query result type (expected Vector): %s", result.Type())
+		c <- statresp
+		return
+	}
+	if len(resultVector) == 0 {
+		resp["successRate"] = nil
+	} else {
+		resp["successRate"] = resultVector
+	}
+	statresp.result = resp
+	c <- statresp
+	return
+}
+
+// processInboundStats handles building the stats information for edges and nodes
+func processInboundStats(nodes []Node, stats map[string]model.Vector) ([]Node, error) {
+
+	for j := range nodes {
+		metrics := getMetrics(nodes[j].App, nodes[j].Version, stats)
+		nodes[j].Stats = metrics
+	}
+	return nodes, nil
+}
+
+func processOutboundStats(edges []Edge, stats map[string]model.Vector) ([]Edge, error) {
+
+	for i := range edges {
+		metrics := getMetrics(edges[i].FromApp, edges[i].FromVersion, stats)
+		edges[i].Stats = metrics
+	}
+	return edges, nil
+}
+
+func getMetrics(app, version string, stats map[string]model.Vector) map[string]float64 {
+	//TODO use const here
+	metrics := [5]string{"p50ms", "p90ms", "p99ms", rps, successRate}
+
+	result := map[string]float64{}
+	//var wg sync.WaitGroup
+	sema := sync.Mutex{}
+	for _, metric := range metrics {
+		samples := stats[metric]
+		//wg.Add(1)
+		//go func(samples model.Vector, metric string, sema *sync.Mutex, wg *sync.WaitGroup) {
+		for _, sample := range samples {
+			appVar, ok := sample.Metric["app"]
+			if !ok || string(appVar) != app {
+				continue
+			}
+			verVar, ok := sample.Metric["version"]
+			if !ok || string(verVar) != version {
+				continue
+			}
+			logrus.Debugf("found %s for app: %s version: %s", metric, app, version)
+			sema.Lock()
+			result[metric] = float64(sample.Value)
+			sema.Unlock()
+			//wg.Done()
+		}
+		//}(samples, metric, &sema, &wg)
+
+	}
+	//wg.Wait()
+	return result
 }
